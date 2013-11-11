803bc94d868670fc9278196ca149eb6e6eaa15d3
jsadiga <jsadiga@gmail.com>
2013-11-11 18:52:08 -0500

    dev2

--- chap6.py
+++ chap6.py
@@ -1,9 +1,68 @@
 # Enter your answrs for chapter 6 here
-# Name:
+# Name:shishira
 
 
 # Ex. 6.6
+# def first(word):
+# 	return word[0]
 
+# def last(word):
+# 	return word[-1]
+
+# def middle(word):
+# 	return word[1:-1]
+
+# print middle("shishira")
+
+# def palindrome(word):
+# 	newword=word[::-1]
+# 	print newword
+# 	if newword==word:
+# 		return True
+# 	else:
+# 		return False
+
+# print palindrome("noon")
 
 
 # Ex 6.8
+
+def gcd(a,b):
+	if b==0:
+		return a
+	elif b>=a:
+		return "a needs to be greater than b"
+	else:
+		c=a%b
+		return gcd(b,c)
+		# if c==0:
+		# 	return b
+		# else:
+			# gcd(b,c)
+	
+
+
+print gcd(66,8)
+
+			
+	
+
+	
+		
+		
+
+
+
+		
+			
+
+
+		
+			
+	
+			
+
+
+
+
+	
--- chap7.py
+++ chap7.py
@@ -1,8 +1,33 @@
 # Enter your answrs for chapter 7 here
-# Name:
+# Name:shishira
 
 
 # Ex. 7.5
+import math
+def factorial(n):
+	if n==0:
+		return 1
+	else:
+		recurse = factorial(n-1)
+		result = n*recurse
+		return result
+
+
+def estimate_pi():
+	x=((2*math.sqrt(2))/9801.0)
+	while True:
+		k=0
+		f=4*k	
+		y=(factorial(k)**4)*(396**f)
+		n=(factorial(f))*(1103.0+(26390*k))
+		pi=1.0/(x*(n/y))
+		k=k+1
+		if pi<1e-15:
+			break
+			return k
+		
+			
+print estimate_pi()		
 
 
 
